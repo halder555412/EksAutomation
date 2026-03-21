e042a10c856ea6ff12b303679dd6c380668cf644
Sajal <sajal18041998@gmail.com>
2026-03-21 20:38:09 +0530

    Initial commit

---
+++ src/test/java/com/sh/EksAutomation/EksAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.EksAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
