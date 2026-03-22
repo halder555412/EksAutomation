fc33134523e8931238474dd981ac98d53284a3ec
Sajal <sajal18041998@gmail.com>
2026-03-22 21:19:20 +0530

    Update docker file

--- src/main/java/com/sh/EksAutomation/controller/HelloController.java
+++ src/main/java/com/sh/EksAutomation/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/test")
     public String home() {
-        return "Hello from EksAutomation test AWS deployment";
+        return "Hello from EksAutomation test AWS deployment after changing the port";
     }
 }
